9ef810ba18be9cd3a908ed7a4490cd217dab274d
Matic Štucin <matic.stucin@gmail.com>
2025-07-21 21:22:01 +0200

    Update uvoz_podatkov.sql

--- SQL Skripte/uvoz_podatkov.sql
+++ SQL Skripte/uvoz_podatkov.sql
@@ -7,6 +7,13 @@ LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (ime, priimek, email, telefon, naslov, posta, kraj, spol, interest, status);
 
+LOAD DATA INFILE '/var/lib/mysql-files/podatki_crm.csv'
+INTO TABLE podatki_crm
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(ck_sif, ck_podr, ck_uspr, ck_dspr, ck_zaup13, ime, priimek, email, telefon, naslov, posta, kraj, spol, interest);
 
 
 
